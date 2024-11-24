d18f1b07dbc29b219fdef5c0ef84f91550238137
Yash Garg <yashgarg@goodeducator.com>
2024-11-24 22:26:36 +0530

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template',
+  base: '/',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
@@ -25,6 +25,11 @@ export default defineConfig({
   server: {
     // Exposes your dev server and makes it accessible for the devices in the same network.
     host: true,
+    port: 4173, // Optional: Ensures the server binds to the right port
+  },
+  preview: {
+    host: '0.0.0.0', // Required to allow Render to access the preview server
+    port: 4173,      // Matches Render's detected port
   },
 });
 
